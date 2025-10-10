f289c7b8bb150b863f27bf56d55140a9d7f85aae
Jack Han <jack.han@amd.com>
2025-10-10 10:32:26 +0900

    Refactored to identify rocm version installed in target container

--- run_vllm_benchmark.py
+++ run_vllm_benchmark.py
@@ -210,8 +210,19 @@ class VLLMBenchmark:
         if self.env_vars.get('QUANTIZATION', 'auto') != 'auto':
             args.extend(["--quantization", f"{self.env_vars.get('QUANTIZATION')}"])
         if self.env_vars.get('ROCM_VERSION', ''):
-            # ROCM_VERSION format should be like "5.4.3" or "5.5.0"
-            rocm_version_nums = [int(x) for x in re.findall(r'\d+', self.env_vars.get('ROCM_VERSION', ''))]
+            # ROCM_VERSION format should be like "6.4.0" or "7.0.1"
+            # load rocm version from pytorch installed in container
+            container_rocm_version = subprocess.run(
+                ["docker", "run", "--rm", self.vllm_image, "python3", "-c", 
+                 "import torch; print(torch.version.hip)"],
+                capture_output=True, text=True
+            ).stdout.strip()
+            if container_rocm_version:
+                self.env_vars['ROCM_VERSION'] = container_rocm_version
+            else:
+                logger.warning("Failed to get ROCM version from container")
+
+            rocm_version_nums = [int(x) for x in re.findall(r'\d+', container_rocm_version)]
             if len(rocm_version_nums) >= 2:
                 if (rocm_version_nums[0] >= 7 and rocm_version_nums[1] >= 0):
                     args.append("--async-scheduling")
