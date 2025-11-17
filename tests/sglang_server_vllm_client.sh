5f6dc87854b33e02f05d0c495818150a4889fa39
Jack Han <jack.han@amd.com>
2025-11-18 00:48:18 +0900

    fixed. execution command

--- tests/sglang_server_vllm_client.sh
+++ tests/sglang_server_vllm_client.sh
@@ -4,7 +4,7 @@ python3 main.py \
     --model-config configs/models/llama-sglang.yaml \
     --model-path-or-id amd/Llama-3.1-8B-Instruct-FP8-KV \
     --backend sglang \
-    --sglang-image sglang/sglang-llm:latest \
+    --image sglang/sglang-llm:latest \
     --benchmark-client vllm \
     --test-plan sample \
     --gpu-devices 0
--- tests/vllm_server_genai_perf_client.sh
+++ tests/vllm_server_genai_perf_client.sh
@@ -4,7 +4,7 @@ python3 main.py \
     --model-config configs/models/llama-vllm.yaml \
     --model-path-or-id amd/Llama-3.1-8B-Instruct-FP8-KV \
     --backend vllm \
-    --vllm-image docker.io/rocm/vllm:latest \
+    --image docker.io/rocm/vllm:latest \
     --benchmark-client genai-perf \
     --test-plan sample \
     --gpu-devices 0
