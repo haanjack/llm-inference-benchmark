aa50c2af360c5f1f6b49512b7d8d26bb507d6457
Jack Han <jack.han@amd.com>
2025-11-17 17:53:19 +0900

    Updated. make the test simple

--- tests/sglang_test.sh
+++ tests/sglang_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python3 main.py \
-    --model-config configs/models/gpt-oss.yaml \
-    --model-path-or-id openai/gpt-oss-120b \
+    --model-config configs/models/llama.yaml \
+    --model-path-or-id amd/Llama-3.1-8B-Instruct-FP8-KV \
     --backend sglang \
     --sglang-image sglang/sglang-llm:latest \
     --test-plan sample \
--- tests/vllm_test.sh
+++ tests/vllm_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python3 main.py \
-    --model-config configs/models/gpt-oss.yaml \
-    --model-path-or-id openai/gpt-oss-120b \
+    --model-config configs/models/llama.yaml \
+    --model-path-or-id amd/Llama-3.1-8B-Instruct-FP8-KV \
     --backend vllm \
     --vllm-image docker.io/rocm/vllm:rocm7.0.0_vllm_0.11.1_20251103 \
     --test-plan sample \
